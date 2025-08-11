92d318bb89226fd7779d09c5136dcd6a610546bc
gfaim3 <140301797+gfaim3@users.noreply.github.com>
2025-08-11 15:47:06 +1000

    Finished Writing all the edits to the features only need specifics

--- frontend/src/components/Explanationcard.jsx
+++ frontend/src/components/Explanationcard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default function Explanationcard({ title, colour }) {
+export default function Explanationcard({ title, colour, children }) {
   return (
     <article className={`w-full bg-${colour} h-100 rounded-lg p-4 text-white`}>
-      <h1 className="text-4xl font-bold mb-2 text-center font-sans">{title}</h1>
+      <h1 className="text-4xl font-bold text-center font-sans">{title}</h1>
+      {children}
     </article>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import Explanationcard from "../components/Explanationcard";
 import Picbutton from "../components/Picbutton";
 import { useNavigate } from "react-router-dom";
+import Dash from "../../public/dashboard.svg";
+import Stats from "../../public/stats.svg";
+import Predictions from "../../public/predictions.svg";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -32,9 +35,28 @@ export default function Home() {
         </div>
 
         {/* section divider */}
-        <div className="w-full border-b-4 border-white mb-6" />
+        <div className="w-full border-b-4 border-white mb-6"/>
 
-        <Explanationcard title="Features" colour="amber-600/80">
+        <Explanationcard title="Features" colour="amber-600">
+          <div className="w-full grid grid-cols-1 sm:grid-cols-3 gap-6 mt-12">
+            <div className="border-4 border-gray-400 rounded-lg p-4 flex flex-col items-center">
+              <img src={Stats} alt="Stats icon" className="w-20 h-20 mb-2" />
+              <p className="text-center font-semibold">Historical Ratings</p>
+              <p> Writing about Historical Ratings </p>
+            </div>
+
+            <div className="border-4 border-gray-400 rounded-lg p-4 flex flex-col items-center">
+              <img src={Dash} alt="Dashboard icon" className="w-20 h-20 mb-2" />
+              <p className="text-center font-semibold">Statistics Dashboard</p>
+              <p>Writing about Statistics Dashboard</p>
+            </div>
+
+            <div className="border-4 border-gray-400 rounded-lg p-4 flex flex-col items-center">
+              <img src={Predictions} alt="Predictions icon" className="w-20 h-20 mb-2"/>
+              <p className="text-center font-semibold">Predictions</p>
+              <p>Writing about Predictions</p>
+            </div>
+          </div>
         </Explanationcard>
       </main>
     </div>
