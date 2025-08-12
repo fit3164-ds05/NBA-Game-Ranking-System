71f07926d4bfffb7cbae36e428a4edf484fb5a9f
gfaim3 <140301797+gfaim3@users.noreply.github.com>
2025-08-12 11:36:13 +1000

    got rid of problems on About page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,32 +4,8 @@ import axios from 'axios';
 
 function About() {
 
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchAPI = async () => {
-      try {
-        const res   = await fetch("/api");   // proxied to Flask
-        const data  = await res.json();      // <-- parse body
-        setUsers(data.users);                // ["User1", ]
-      } catch (err) {
-        console.error("API error:", err);
-      }
-    };
-    fetchAPI();
-  }, []);
-
   return (
-
-    <div>
-    {users.map((u, idx) => (
-      <div key={idx}>
-        <span>{u}</span>
-        <br />               
-      </div>
-    ))}
-  </div>
-  )
+  <div>About Page</div>)
 }
 
 export default About
